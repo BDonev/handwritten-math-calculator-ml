5d64ce42096ee1f0f8f6da1c538953e31ccb093b
BDonev <borislav.donev@gmail.com>
2019-02-27 18:19:02 +0200

    Modifying training scripts

--- train_math_symbols.py
+++ train_math_symbols.py
@@ -5,7 +5,14 @@ from keras.layers import Activation, Dropout, Flatten, Dense
 from keras.optimizers import Adam
 from keras import backend as K
 from keras.layers.normalization import BatchNormalization
+from keras.utils import plot_model
+
 import numpy as np
+import os
+
+def get_samples_count(data_dir):
+    files_count = sum([len(files) for r, d, files in os.walk(data_dir)])
+    return files_count
 
 # dimensions of our images.
 img_width, img_height = 45, 45
@@ -13,11 +20,15 @@ channels = 1
 
 train_data_dir = 'data/math_symbols/train'
 validation_data_dir = 'data/math_symbols/test'
-nb_train_samples = 3000
-nb_validation_samples = 800
-epochs = 50
-batch_size = 16
+train_samples = get_samples_count(train_data_dir)
+validation_samples = get_samples_count(validation_data_dir)
+print("Number of training samples: " + str(train_samples))
+print("Number of validation samples: " + str(validation_samples))
+epochs = 12
+batch_size = 64
 classes_count = 5
+train_iterations = train_samples / batch_size
+validation_iterations = validation_samples / batch_size
 
 if K.image_data_format() == 'channels_first':
     input_shape = (channels, img_width, img_height)
@@ -52,7 +63,7 @@ cnn.compile(optimizer=Adam(lr=0.001),
 
 # this is the augmentation configuration we will use for training
 train_datagen = ImageDataGenerator(
-    rescale=1./127,
+    rescale=1./255,
     rotation_range=5,
     width_shift_range=0.2,
     height_shift_range=0.2,
@@ -62,7 +73,7 @@ train_datagen = ImageDataGenerator(
 
 # this is the augmentation configuration we will use for testing:
 # only rescaling
-test_datagen = ImageDataGenerator(rescale=1./127)
+test_datagen = ImageDataGenerator(rescale=1./255)
 
 train_generator = train_datagen.flow_from_directory(
     train_data_dir,
@@ -81,11 +92,18 @@ validation_generator = test_datagen.flow_from_directory(
 cnn.fit_generator(
     train_generator,
     verbose=1,
-    steps_per_epoch=nb_train_samples // batch_size,
+    steps_per_epoch=train_iterations,
     epochs=epochs,
     validation_data=validation_generator,
-    validation_steps=nb_validation_samples // batch_size)
+    validation_steps=validation_iterations)
 
 label_map = (train_generator.class_indices)
-np.save('server/label_map_math_symbols.npy', label_map)
-cnn.save('server/model_math_symbols.h5')
+np.save('label_map_math_symbols.npy', label_map)
+cnn.save('model_math_symbols.h5')
+
+score = cnn.evaluate_generator(validation_generator, steps=validation_iterations, verbose=1)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+print(cnn.summary())
+
+plot_model(cnn, to_file='model_math_symbols.png')
--- train_mnist.py
+++ train_mnist.py
@@ -6,14 +6,15 @@ Gets to 99.25% test accuracy after 12 epochs
 '''
 
 import keras
+from keras.utils import plot_model
 from keras.datasets import mnist
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Activation, Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
-from keras import backend as K
+from keras.optimizers import Adam
 
 batch_size = 128
-num_classes = 10
+classes_count = 10
 epochs = 12
 
 # input image dimensions
@@ -35,31 +36,39 @@ print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 
 # convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
+y_train = keras.utils.to_categorical(y_train, classes_count)
+y_test = keras.utils.to_categorical(y_test, classes_count)
 
-model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3),
-                 activation='relu',
-                 input_shape=input_shape))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(num_classes, activation='softmax'))
+cnn = Sequential()
+cnn.add(Conv2D(32, kernel_size=(3, 3), input_shape=input_shape))
+cnn.add(Activation('relu'))
 
-model.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer=keras.optimizers.Adadelta(),
-              metrics=['accuracy'])
+cnn.add(Conv2D(64, (3, 3)))
+cnn.add(Activation('relu'))
+cnn.add(MaxPooling2D(pool_size=(2, 2)))
+cnn.add(Dropout(0.25))
 
-model.fit(x_train, y_train,
+cnn.add(Flatten())
+cnn.add(Dense(128))
+cnn.add(Activation('relu'))
+cnn.add(Dropout(0.5))
+
+cnn.add(Dense(classes_count))
+cnn.add(Activation('softmax'))
+
+cnn.compile(optimizer=Adam(lr=0.001),
+            loss='categorical_crossentropy',
+            metrics=['accuracy'])
+
+cnn.fit(x_train, y_train,
           batch_size=batch_size,
           epochs=epochs,
           verbose=1,
           validation_data=(x_test, y_test))
-score = model.evaluate(x_test, y_test, verbose=0)
+score = cnn.evaluate(x_test, y_test, verbose=1)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
-model.save('server/model_mnist.h5')
+print(cnn.summary())
+cnn.save('model_mnist.h5')
+
+plot_model(cnn, to_file='model_mnist.png')
